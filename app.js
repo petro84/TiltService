3cee6d8a65a87a73901d29df51bfa096e37cf365
Larry Petroski <larry@Petro.local>
2019-04-20 15:51:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const noble_mac = require('noble-mac')
+const noble = require('noble')
+
+const Tilt = require('./lib/tilt-scanner')
+const database = require('./lib/database')
+
+let scanner = undefined
+let stopped = false
+debugger
+
+if (process.platform === 'darwin') {
+    scanner = new Tilt(noble_mac)
+} else {
+    scanner = new Tilt(noble)
+}
+
+scanner.startScan()
+    .then(() => console.log('Scanning...'))
+    .catch((e) => console.log(e))
+
+scanner.onAdvertisement = (data) => {
+    if (data) {
+        scanner.stopScan()
+        database.ref(`tilt/${data.color}/beacon`).push(data).then(() => stopped = true)
+    }
+}
+
+setInterval(() => {
+    if (stopped) {
+        scanner.startScan()
+            .then(() => stopped = false)
+            .catch((e) => console.log(e))
+    }
+}, 60000)
