3909a907c3d3838e6d2d7194ebbabf51618cd6b1
Larry Petroski <larry@Petro.local>
2019-12-07 14:53:28 -0600

    Uses express routes and a dedicated port

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # TiltService
 
-Service is intended to run on raspberry pi device(s) as a startup service. Two main modules have been imported
-* Noble (used on raspberry pi device)
-* Noble-Mac (used on mac for development/testing purposes)
+Service is intended to run on raspberry pi device(s) as a startup service. Two main routes exist to be called from front-end UI to start and stop bluetooth scanning.
+
+Modules used:
+@abandonware/noble
+express
+firebase-admin
 
 The filtering logic was attained from the node-beacon-scanner project. To better understand what it was doing, I rewrote it to satisfy my needs (i.e. only grab certain pieces of info and not everything related to the beacon)
-
-Logging beacons/data to Firebase database, hence, third a final npm module.
 
 To run service automatically on raspberry pi, copy tilt.service file to /etc/systemd/system folder on pi. Then run the following:
 * sudo systemctl enable tilt.service (registers new service with os boot logic)
--- app.js
+++ app.js
@@ -1,38 +1,82 @@
-const noble_mac = require('noble-mac')
-const noble = require('noble')
+const express = require('express')
 
-const Tilt = require('./lib/tilt-scanner')
-const database = require('./lib/database')
+const scanner = require('./lib/tilt-scanner')
+const {saveBeacon, getInterval} = require('./lib/database')
 
-let scanner = undefined
-let stopped = false
-let interval = undefined
+const scan = new scanner()
+const app = express()
 
-if (process.platform === 'darwin') {
-    scanner = new Tilt(noble_mac)
-} else {
-    scanner = new Tilt(noble)
-}
+let stopped = true
+let intervals = undefined
 
-database.ref('tilt/Settings').on('value',(snapshot) => {
-    interval = snapshot.val()
+app.post('/Start', (req, res) => {
+    startScan()
+
+    res.send('Running!')
 })
 
-scanner.startScan()
-    .then(() => console.log('Scanning...'))
-    .catch((e) => console.log(e))
+app.post('/Stop', (req, res) => {
+    clearInterval(intervals)
+    intervals = undefined
 
-scanner.onAdvertisement = (data) => {
-    if (data) {
-        scanner.stopScan()
-        database.ref(`tilt/${data.color}/beacon`).push(data).then(() => stopped = true)
-    }
+    res.send('Stopped!')
+})
+
+app.get('*', (req, res) => res.status(404).send())
+
+app.listen(5841, () => console.log('Listening on port 5841'))
+
+scan.onAdvertisement = (beacon) => {
+    scan.stopScan()
+
+    saveBeacon(beacon).then(() => stopped = true)
 }
 
-setInterval(() => {
-    if (stopped) {
-        scanner.startScan()
-            .then(() => stopped = false)
-            .catch((e) => console.log(e))
-    }
-}, interval)
+const startScan = async () => {
+    let interval = await getInterval()
+
+    intervals = setInterval(() => {
+        if (stopped) {
+            scan.startScan().then(() => stopped = false)
+        }
+    }, interval)
+}
+
+// const noble_mac = require('noble-mac')
+// const noble = require('noble')
+
+// const Tilt = require('./lib/tilt-scanner')
+// const database = require('./lib/database')
+
+// let scanner = undefined
+// let stopped = false
+// let interval = undefined
+
+// if (process.platform === 'darwin') {
+//     scanner = new Tilt(noble_mac)
+// } else {
+//     scanner = new Tilt(noble)
+// }
+
+// database.ref('tilt/Settings').on('value',(snapshot) => {
+//     interval = snapshot.val()
+// })
+
+// scanner.startScan()
+//     .then(() => console.log('Scanning...'))
+//     .catch((e) => console.log(e))
+
+// scanner.onAdvertisement = (data) => {
+//     if (data) {
+//         scanner.stopScan()
+//         database.ref(`tilt/${data.color}/beacon`).push(data).then(() => stopped = true)
+//     }
+// }
+
+// setInterval(() => {
+//     if (stopped) {
+//         scanner.startScan()
+//             .then(() => stopped = false)
+//             .catch((e) => console.log(e))
+//     }
+// }, interval)
