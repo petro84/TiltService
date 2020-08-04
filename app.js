096e92c3a6313e8d588d71d933023e07374c5298
Larry Petroski <larry@Petro.local>
2020-08-04 10:43:46 -0500

    modified database structure

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ let stopped = true
 let docName = undefined
 
 // listen for changes in database
-database.ref('settings').on('value', (snapshot) => {
+database.ref('tilt/settings').on('value', (snapshot) => {
     processData(snapshot.val());
 })
 
-const processData = ({interval, name, status}) => {
+const processData = ({interval, name, active}) => {
     // Stop Scanning
     clearInterval(intervals)
     intervals = undefined
 
     // Setup new document in database (i.e. tilt/name/beacons)
-    docName = (name == '' ? undefined : `tilt/${name}/beacons`)
+    docName = (name == '' ? undefined : `tilt/beacons/${name}`)
 
     // Start scanning if active (in milliseconds)
-    if (status == 'active' && docName) {
+    if (active && docName) {
         intervals = setInterval(() => {
             if (stopped) {
                 scan.startScan().then(() => stopped = false)
